38abc6575dbe9b6f621edaa7c9e5b7bf60a5b268
OssieFromDK <mrcraftonyt@gmail.com>
2026-01-02 17:26:19 -0600

    Create config.js

---
+++ config.js
@@ -0,0 +1,13 @@
+// config.js
+const SUPABASE_URL = 'https://kleonejdkmalzkgehhgt.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtsZW9uZWpka21hbHprZ2VoaGd0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjczODc1MjksImV4cCI6MjA4Mjk2MzUyOX0.5YmLdNK_-1ZydX6tVOXBFMmv9IChj8Ta8O-WLYERDQw';
+
+// Initialize Supabase client
+const supabase = window.supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
+  auth: {
+    persistSession: true,
+    autoRefreshToken: true
+  }
+});
+
+export { supabase };
